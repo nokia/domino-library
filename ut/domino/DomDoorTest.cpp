37146a75b711f4d74b7f8b888a6a105bee3b87b5
fchn289 <sz.chen@nokia-sbell.com>
2022-09-28 16:21:51 +0800

    make run-ut_exe

---
+++ ut/domino/DomDoorTest.cpp
@@ -0,0 +1,19 @@
+/**
+ * Copyright 2018-2022 Nokia
+ * Licensed under the BSD 3 Clause license
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// ***********************************************************************************************
+#include <gtest/gtest.h>
+
+#include "DomDoor.hpp"
+
+using namespace testing;
+
+namespace RLib
+{
+// ***********************************************************************************************
+TEST(DomDoorTest, GOLD_1st)
+{
+}
+}  // namespace
