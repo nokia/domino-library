04979aad9e4d050dd1bc7f3d4702c349fdf158a3
fchn289 <sz.chen@nokia-sbell.com>
2023-07-14 08:50:16 +0800

    MsgSelf: copilot compare#2

--- src/msg_self/MsgSelf.cpp
+++ src/msg_self/MsgSelf.cpp
@@ -25,11 +25,10 @@ MsgSelf::~MsgSelf()
 // ***********************************************************************************************
 void MsgSelf::handleAllMsg(const shared_ptr<bool> aValidMsgSelf)
 {
-    if (*aValidMsgSelf)          // impossible aValidMsgSelf==nullptr till 022-Mar-11
-    {
-        // may be called after MsgSelf destructed, not use this at all
-        HID("(MsgSelf) How many reference to this MsgSelf? " << aValidMsgSelf.use_count());
+    HID("(MsgSelf) How many reference to this MsgSelf? " << aValidMsgSelf.use_count());
 
+    if (*aValidMsgSelf)  // impossible aValidMsgSelf==nullptr since 022-Mar-11
+    {
         if (not hasMsg())
             return;
         while (handleOneMsg());  // handleOneMsg() may create new high priority msg(s)
@@ -50,11 +49,11 @@ bool MsgSelf::handleOneMsg()
         --nMsg_;
 
         if (not hasMsg())
-            return false;
+            return false;    // no more to continue
         if (isLowPri(EMsgPriority(msgPri)))
         {
             pingMainFN_([this, isValid = isValid_]() mutable { handleAllMsg(isValid); });
-            return false;  // 1 low msg per loopReq so queue with eg IM CB, syscom, etc. (lower)
+            return false;    // not continue for low priority until next ping-pong
         }
         return true;
     }
@@ -72,9 +71,8 @@ void MsgSelf::newMsg(const MsgCB& aMsgCB, const EMsgPriority aMsgPri)
 
     msgQueues_[aMsgPri].push(aMsgCB);
     ++nMsg_;
-    if (nMsg_ > 1)
-        return;
 
-    pingMainFN_([this, isValid = isValid_]() mutable { handleAllMsg(isValid); });
+    if (nMsg_ == 1)  // ist MsgCB
+        pingMainFN_([this, isValid = isValid_]() mutable { handleAllMsg(isValid); });
 }
 }  // namespace
