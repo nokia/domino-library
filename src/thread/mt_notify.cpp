a567f050fdbe4a39b2f290da347ca5baf0c30d7f
fchn289 <sz.chen@nokia-sbell.com>
2023-10-25 10:35:01 +0800

    add mt_notify

---
+++ src/thread/mt_notify.cpp
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2022 Nokia. All rights reserved.
+ * Licensed under the BSD 3 Clause license
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#include "mt_notify.hpp"
+
+namespace RLib
+{
+MT_Semaphore g_sem;
+}  // namespace
---
+++ src/thread/mt_notify.hpp
@@ -0,0 +1,32 @@
+/**
+ * Copyright 2023 Nokia. All rights reserved.
+ * Licensed under the BSD 3 Clause license
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+// ***********************************************************************************************
+// - Why/VALUE:
+//   . help eg MsgSelf, ThreadBack, MtInQueue to wakeup main thread's mt_wait()
+//     . simpler than para to constructing above class(es)
+//   . can base on MT_Semaphore
+//     . can be empty so no wakeup at all
+//     . this file is an example, users can define their own mt_notify.hpp
+// ***********************************************************************************************
+#pragma once
+
+#include "MT_Semaphore.hpp"
+
+namespace RLib
+{
+extern MT_Semaphore g_sem;  // can't use ObjAnywhere that is not MT safe
+// ***********************************************************************************************
+inline void mt_notify()
+{
+    g_sem.mt_notify();
+}
+
+}  // namespace
+// ***********************************************************************************************
+// YYYY-MM-DD  Who       v)Modification Description
+// ..........  .........   .......................................................................
+// 2023-10-25  CSZ       1)create
+// ***********************************************************************************************
