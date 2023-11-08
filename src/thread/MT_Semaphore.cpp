a84a13a5865151adabe28dc12ca0998737cfae0a
fchn289 <sz.chen@nokia-sbell.com>
2023-11-08 15:12:34 +0800

    sem: rm impossible err handling

--- src/thread/MT_Semaphore.cpp
+++ src/thread/MT_Semaphore.cpp
@@ -16,18 +16,20 @@ namespace RLib
 void MT_Semaphore::mt_notify()
 {
     int count = 0;
-    sem_getvalue(&mt_sem_, &count);  // impossible failed according to man page
+    sem_getvalue(&mt_sem_, &count);  // impossible failed since MT_Semaphore's constructore
     if (count > 0)  // >0 to avoid count overflow; timeout is deadline
         return;
-    sem_post(&mt_sem_);  // impossible failed according to man page
+    sem_post(&mt_sem_);  // impossible failed since MT_Semaphore's constructor
 }
 
 // ***********************************************************************************************
 void MT_Semaphore::timedwait(const size_t aSec, const size_t aRestNsec)
 {
     timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) == -1)
-        return;
+    ts.tv_sec = 0;
+    ts.tv_nsec = 0;
+    clock_gettime(CLOCK_REALTIME, &ts);  // impossible failed since MT_Semaphore's constructor
+
     const auto ns = ts.tv_nsec + aRestNsec;
     ts.tv_sec += (aSec + ns / 1000'000'000);
     ts.tv_nsec = ns % 1000'000'000;
