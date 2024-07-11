16cd78bdcaad35356c065446a58cfa6b7756ce27
fchn289 <sz.chen@nokia-sbell.com>
2024-07-11 12:17:47 +0800

    ThPoolBack: inc cov

--- ut/thread/ThPoolBackTest.cpp
+++ ut/thread/ThPoolBackTest.cpp
@@ -15,8 +15,6 @@ TEST_F(ThPoolBackTest, invalid_maxThread)
         [](bool) {}  // backFn
     )) << "REQ: can create new task";
     while (threadBack_.hdlFinishedTasks() == 0);  // REQ: wait new task done
-
-    myPool.fakeNotify();  // simulate wrong notify; inc cov
 }
 
 // ***********************************************************************************************
@@ -24,29 +22,31 @@ TEST_F(ThPoolBackTest, performance)
 {
     const size_t maxThread = 10;  // github ci can afford 100 (1000+ slower than belinb03)
 
-    AsyncBack asyncBack;
-    auto start = high_resolution_clock::now();
-    for (size_t i = 0; i < maxThread; i++)
-        EXPECT_TRUE(asyncBack.newTaskOK(
-            [] { return true; },  // entryFn
-            [](bool) {}  // backFn
-        ));
-    for (size_t nHandled = 0; nHandled < maxThread; nHandled += asyncBack.hdlFinishedTasks());
-    auto dur = duration_cast<chrono::microseconds>(high_resolution_clock::now() - start);
-    HID("AsyncBack cost=" << dur.count() << "us");
-
     ThPoolBack thPoolBack(maxThread);
-    start = high_resolution_clock::now();
+    auto start = high_resolution_clock::now();
     for (size_t i = 0; i < maxThread; i++)
         EXPECT_TRUE(thPoolBack.newTaskOK(
             [] { return true; },  // entryFn
             [](bool) {}  // backFn
         ));
     for (size_t nHandled = 0; nHandled < maxThread; nHandled += thPoolBack.hdlFinishedTasks());
-    dur = duration_cast<chrono::microseconds>(high_resolution_clock::now() - start);
+    auto dur = duration_cast<chrono::microseconds>(high_resolution_clock::now() - start);
     HID("ThPoolBack cost=" << dur.count() << "us");
     // - belinb03 : ~ 10 faster than AsyncBack
     // - github ci: ~100 faster than AsyncBack
+
+    thPoolBack.fakeNotify();  // simulate wrong notify; inc cov
+
+    AsyncBack asyncBack;
+    start = high_resolution_clock::now();
+    for (size_t i = 0; i < maxThread; i++)
+        EXPECT_TRUE(asyncBack.newTaskOK(
+            [] { return true; },  // entryFn
+            [](bool) {}  // backFn
+        ));
+    for (size_t nHandled = 0; nHandled < maxThread; nHandled += asyncBack.hdlFinishedTasks());
+    dur = duration_cast<chrono::microseconds>(high_resolution_clock::now() - start);
+    HID("AsyncBack cost=" << dur.count() << "us");
 }
 
 }  // namespace
