fdbac17dbe184d06d0aac4154a0cd4f8e17d79b6
fchn289 <csz289@aliyun.com>
2026-08-12 18:52:33 -0500

    typo; safer SafePtr->()

--- src/thread/MtInQueue.cpp
+++ src/thread/MtInQueue.cpp
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 // ***********************************************************************************************
+#include <algorithm>
+
 #include "MtInQueue.hpp"
 #include "MT_PingMainTH.hpp"
 
